13354425ee59b10f3c22bbc442fdd85db2aa9d17
thegreenjag1 <thegreenjag1@gmail.com>
2017-12-21 17:23:47 -0600

    added json support. Should reduce app size

--- Manna/Pages/MenuPage.js
+++ Manna/Pages/MenuPage.js
@@ -1,14 +1,25 @@
 var Observable = require("FuseJS/Observable");
 
 var sidebarOpen = Observable(false);
+var cardsMenu = Observable();
+
+fetch("https://api.myjson.com/bins/125jfj")
+.then(function(result) {
+	result.json().then(function(data) {
+		for (var i = 0; i < 10; i++) {
+			var item = data[i];
+			cardsMenu.add(item);
+		}
+	});
+});
 
 
 function setSidebarOpen() {
-    sidebarOpen.value = true;
+	sidebarOpen.value = true;
 };
 
 function setSidebarClosed() {
-    sidebarOpen.value = false;
+	sidebarOpen.value = false;
 };
 
 function goBack() {
@@ -24,5 +35,6 @@ module.exports = {
 	sidebarOpen: sidebarOpen,
 	setSidebarClosed: setSidebarClosed,
 	setSidebarOpen: setSidebarOpen,
-	goToContactUsPage: goToContactUsPage
+	goToContactUsPage: goToContactUsPage,
+	cardsMenu: cardsMenu
 };
